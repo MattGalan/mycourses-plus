b9ea377dca1fb4b72edc1040d49fd9d9129be45d
MatthewGalan <mattg54321@gmail.com>
2020-11-03 17:37:20 -0500

    rewrite element hider for home page

---
+++ hide_useless/homepage.js
@@ -0,0 +1,12 @@
+// Remove nav footer
+$(".d2l-navigation-s-main-wrapper").remove();
+
+// Remove instructions panel
+$(
+  "body > div.d2l-page-main.d2l-max-width.d2l-min-width.d2l-home > div.d2l-page-main-padding > div.d2l-homepage > div > div.homepage-row > div > div.homepage-col-8 > div:nth-child(1)"
+).remove();
+
+// Remove user links
+$(
+  "body > div.d2l-page-main.d2l-max-width.d2l-min-width.d2l-home > div.d2l-page-main-padding > div.d2l-homepage > div > div.homepage-row > div > div.homepage-col-4 > div:nth-child(1)"
+).remove();
