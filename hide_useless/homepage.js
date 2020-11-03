4b16f0cd08ea4ca7373b6830fbf495c0f1a6b1e3
MatthewGalan <mattg54321@gmail.com>
2020-11-03 17:40:03 -0500

    add blocker for homepage banner

--- hide_useless/homepage.js
+++ hide_useless/homepage.js
@@ -10,3 +10,8 @@ $(
 $(
   "body > div.d2l-page-main.d2l-max-width.d2l-min-width.d2l-home > div.d2l-page-main-padding > div.d2l-homepage > div > div.homepage-row > div > div.homepage-col-4 > div:nth-child(1)"
 ).remove();
+
+// Remove banner message
+$(
+  "body > div.d2l-page-main.d2l-max-width.d2l-min-width.d2l-home > div.d2l-page-main-padding > div.d2l-homepage > div > div:nth-child(1)"
+).remove();
