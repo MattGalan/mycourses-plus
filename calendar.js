0f65e7fb5b2dcdb3bd6f93d4e2070e9d58952ad6
MatthewGalan <mattg54321@gmail.com>
2020-11-05 00:44:27 -0500

    shorten course names in calendar widget

--- calendar.js
+++ calendar.js
@@ -12,3 +12,11 @@ $(
 // Remove padding from calendar widget.
 // For more styling see calendar.css
 $($(".d2l-widget.d2l-tile")[3]).css("padding", 0);
+
+// Replace full course names, such as "IMGS.111.01/L1-L4 - Imaging Science Fundamentals"
+// with short course names, such as "IMGS.111"
+$(
+  "ul > li > div.d2l-datalist-item-content > div > div.d2l-inline > div.d2l-textblock:nth-child(3)"
+).each(function () {
+  this.textContent = this.textContent.match(/^\w{4}\.\d{3}/)[0];
+});
