b4f5bb3a1243ec993fc84aa5784cc4aa15d1fdef
MatthewGalan <mattg54321@gmail.com>
2020-11-25 00:03:01 -0500

    move rit links code to their own files

--- calendar.js
+++ calendar.js
@@ -1,15 +1,3 @@
-// Replace starfish panel with a more useful version with more links to RIT services
-// May be a better place for this, but it's fine here for now
-$(
-  "body > div.d2l-page-main.d2l-max-width.d2l-min-width.d2l-home > div.d2l-page-main-padding > div.d2l-homepage > div > div.homepage-row > div > div.homepage-col-4 > div.d2l-widget.d2l-tile.d2l-custom-widget"
-).html(`
-  <h2>RIT Links</h2>
-  <a href="https://rit.starfishsolutions.com/starfish-ops/instructor/serviceCatalog.html#/" target="_blank">Starfish</a>
-  <a href="https://tigercenter.rit.edu/" target="_blank">Tiger Center</a>
-  <a href="https://campus.ps.rit.edu/psp/PRIT4J/?cmd=login" target="_blank">Student Info System (SIS)</a>
-  <a href="https://eservices.rit.edu/eservices/" target="_blank">eServices</a>
-`);
-
 $(
   "body > div.d2l-page-main.d2l-max-width.d2l-min-width.d2l-home > div.d2l-page-main-padding > div.d2l-homepage > div > div.homepage-row > div > div.homepage-col-4 > div:nth-child(1)"
 ).html("<h2>Upcoming Events</h2><div>Loading events...</div>");
